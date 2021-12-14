d35212873d182891051fd0235080912df77868f9
mkujun <kujundzic.marko@hotmail.com>
2021-12-14 08:20:21 +0100

    day 5, part 2

--- 05.js
+++ 05.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 let input = fs.readFileSync("./inputs/05.txt", 'utf8').split('\n').slice(0, -1);
-//let input = fs.readFileSync("input.txt", 'utf8').split('\n').slice(0, -1);
 
 const parseinputRow = (row) => {
   let inputRow = row.split(" -> ");
@@ -11,9 +10,8 @@ const parseinputRow = (row) => {
   let x2 = inputRow[1].split(",")[0];
   let y2 = inputRow[1].split(",")[1];
 
-
   let line = {
-    x1,y1,x2,y2,crossedNum: 0
+    x1,y1,x2,y2
   }
 
   return line;
@@ -51,7 +49,105 @@ const getPointsOnTheLine = (line) => {
       }
     }
   }
+  
+  // part 2 (45 deg)
+  else{
+    let min = Math.min(x1, x2, y1, y2);
+    let max = Math.max(x1, x2, y1, y2);
+    
+    let slope = (y2 - y1) / (x2 - x1);
 
+    if (slope == -1 || slope == 1) {
+
+      if (x1 == y1 && x2 == y2 ) {
+        for(let i = min; i <= max; i++) {
+          points.push({
+            x: i,
+            y: i
+          })
+        }
+      }
+      
+      else if (x1 == y2 || x2 == y1){
+        let counter1 = min;
+        let counter2 = max;
+        for(let i = min; i <= max; i++){
+          points.push({
+            x: counter2,
+            y: counter1
+          })
+          counter1++;
+          counter2--;
+        }
+      }
+
+      else {
+        if (x1 > x2 && y1 > y2) {
+          let x = x1;
+          let y = y1;
+          do {
+            points.push({x: x,y: y})
+            x--;
+            y--;
+          } while (x !== x2 && y !== y2)
+          points.push({
+            x: x2,
+            y: y2
+          })
+        }
+        else if (x2 > x1 && y2 > y1) {
+          let x = x2;
+          let y = y2;
+          do {
+            points.push({x: x,y: y})
+            x--;
+            y--;
+          } while (x !== x1 && y !== y1)
+          points.push({
+            x: x1,
+            y: y1
+          })
+        }
+
+        else if (x1 < x2 && y1 > y2){
+          let x = x1;
+          let y = y1;
+          do {
+            points.push({
+              x: x,
+              y: y
+            })
+
+            x++;
+            y--;
+          } while (x !== x2 && y !== y1) {
+            points.push({
+              x: x2,
+              y: y2
+            })
+          }
+        }
+        else if (x1 > x2 && y1 < y2) {
+          let x = x1;
+          let y = y1;
+          do {
+            points.push({
+              x: x,
+              y: y
+            })
+
+            x--;
+            y++;
+          } while (x !== x2 && y !== y2) {
+            points.push({
+              x: x2,
+              y: y2
+            })
+          }
+        }
+      }
+    }
+  }
 
   return points;
 }
@@ -81,4 +177,4 @@ mapa.forEach(function(value, key) {
   }
 })
 
-console.log("part 1:", counter);
+console.log("part 2:", counter);
