c1a5a3061c49028efcdcd0feb89de19f15f5a03a
mkujun <kujundzic.marko@hotmail.com>
2021-12-16 11:51:45 +0100

    day 6 part 2

--- 06.js
+++ 06.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-let input = fs.readFileSync("./inputs/06.txt", 'utf8').split(',');
-//let input = [3,4,3,1,2];
+let input = fs.readFileSync("./inputs/06.txt", 'utf8').split(',').map(Number);
 
+// part 1
+/*
 for(let i = 1; i <=80; i++) {
   input.forEach((e, index) => {
 
@@ -16,3 +17,62 @@ for(let i = 1; i <=80; i++) {
 }
 
 console.log(input.length);
+*/
+
+// part 2
+let mapa = new Map();
+
+mapa.set(0, 0);
+mapa.set(1, 0);
+mapa.set(2, 0);
+mapa.set(3, 0);
+mapa.set(4, 0);
+mapa.set(5, 0);
+mapa.set(6, 0);
+mapa.set(7, 0);
+mapa.set(8, 0);
+
+// map filling
+input.forEach(e => {
+  if (mapa.has(e)) {
+    let counter = mapa.get(e);
+    mapa.set(e, counter + 1);
+  }
+})
+
+for(let i = 1; i <=256; i++) {
+  let zero = mapa.get(0);
+  let one = mapa.get(1);
+  let two = mapa.get(2);
+  let three = mapa.get(3);
+  let four = mapa.get(4);
+  let five = mapa.get(5);
+  let six = mapa.get(6);
+  let seven = mapa.get(7);
+  let eight = mapa.get(8);
+
+  mapa.set(0, one);
+  mapa.set(1, two);
+  mapa.set(2, three);
+  mapa.set(3, four);
+  mapa.set(4, five);
+  mapa.set(5, six);
+  mapa.set(6, seven + zero);
+  mapa.set(7, eight);
+
+  mapa.set(8, zero);
+}
+
+let counter = 0;
+counter = 
+  mapa.get(0) + 
+  mapa.get(1) + 
+  mapa.get(2) + 
+  mapa.get(3) + 
+  mapa.get(4) + 
+  mapa.get(5) + 
+  mapa.get(6) + 
+  mapa.get(7) + 
+  mapa.get(8); 
+
+console.log(counter);
