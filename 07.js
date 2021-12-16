b9902b9471e0f13e0e8bfad75990cbf75011cd95
mkujun <kujundzic.marko@hotmail.com>
2021-12-16 15:24:43 +0100

    day 7 part 2

--- 07.js
+++ 07.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 let input = fs.readFileSync("./inputs/07.txt", 'utf8').split(',').map(Number);
-//let input = [16,1,2,0,4,2,7,1,2,14];
 
-let smallest = 100000000;
+let smallest = 10000000000000;
 
+// part 1
+/*
 input.forEach(number => {
   let outcome = 0;
   input.forEach(e => {
@@ -16,5 +17,38 @@ input.forEach(number => {
     smallest = outcome;
   }
 })
+
+console.log("smallest", smallest);
+*/
+
+// part 2
+const defineStep = (distance) => {
+  let step = 0;
+  let value = 0;
+  for (let i = 1; i <=distance; i++) {
+    step = step + 1;
+    value = value + step;
+  }
+
+  return value;
+}
+
+let max = Math.max(...input);
+
+for (let i = 1; i<= max; i++) {
+  let result = 0;
+  input.forEach(number => {
+    let outcome = 0;
+
+    let diff = Math.abs(i - number);
+
+    outcome = outcome + defineStep(diff);
+    result = result + outcome;
+  })
+
+  if (result < smallest) {
+    smallest = result;
+  }
+}
 
 console.log("smallest", smallest);
