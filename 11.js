4d313ae6100f90ac9a2f74d15bb70e4f32935d30
mkujun <kujundzic.marko@hotmail.com>
2022-01-10 19:10:22 +0100

    day 11 part 1

---
+++ 11.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+let input = fs.readFileSync("./inputs/11.txt", 'utf8').split('\n').slice(0, -1);
+//let input = fs.readFileSync("input.txt", 'utf8').split('\n').slice(0, -1);
+let matrix = [];
+let flashes = [];
+let part1 = 0;
+
+input.forEach(row => {
+  matrix.push(row.split('').map(Number));
+})
+
+const print = () => {
+  matrix.forEach(row => {
+    console.log(JSON.stringify(row));
+  })
+}
+
+increaseByOne = () => {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      matrix[i][j] = matrix[i][j] + 1;
+    }
+  }
+}
+
+findFlashes = () => {
+  part1 = part1 + flashes.length;
+  flashes = [];
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      if(matrix[i][j] >= 10) {
+        matrix[i][j] = 0;
+        flashes.push({i,j});
+      }
+    }
+  }
+}
+
+increasePointsAroundFlash = () => {
+  flashes.forEach(flash => {
+    let {i,j} = flash;
+
+    let pointsAround = [];
+
+    pointsAround.push({i: i, j: j - 1})
+    pointsAround.push({i: i, j: j + 1})
+    pointsAround.push({i: i - 1, j: j})
+    pointsAround.push({i: i + 1, j: j})
+    pointsAround.push({i: i - 1, j: j - 1})
+    pointsAround.push({i: i - 1, j: j + 1})
+    pointsAround.push({i: i + 1, j: j - 1})
+    pointsAround.push({i: i + 1, j: j + 1})
+
+    pointsAround.forEach(point => {
+      let {i,j} = point;
+
+      if (i >= 0 && i < 10 && j >=0 && j < 10) {
+        if (matrix[i][j] != 0) {
+          matrix[i][j] = matrix[i][j] + 1;
+        }
+      }
+    })
+  })
+}
+
+for (k = 1; k <= 100; k++) {
+  increaseByOne();
+
+  do{
+    findFlashes();
+    increasePointsAroundFlash();
+  } while (flashes.length > 0)
+}
+
+console.log("result", part1);
